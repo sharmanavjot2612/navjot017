bd342c49d1bc248908942c0a65d221926274628e
sharmanavjot2612 <sharmanavjot2612@gmail.com>
2026-07-17 23:57:35 +0530

    Update Crimeradar.py

--- Crimeradar.py
+++ Crimeradar.py
@@ -420,7 +420,7 @@ elif menu == "📊 Crime Analytics":
         crime_arrest.plot(
             kind="bar",
             stacked=True,
-            color=["#FF4D4D", "#00E676"],
+          color=["#19F0DE", "#23A4BB"]
             edgecolor="white",
             ax=ax
         )
